fcfd9028e55009d1e20e76b4cbd471cf819c4310
anhongkui <anhongkui@jd.com>
2022-12-18 10:35:22 +0800

    onepage

---
+++ src/layout/home.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="banner" id="home"></section>
+</template>
+
+<style scoped>
+section {
+  position: relative;
+  width: 100%;
+  height: 100vh;
+  background: url("../assets/banner.jpg");
+  background-size: cover;
+  background-position: right;
+}
+</style>
